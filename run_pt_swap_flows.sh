a51cea1344fcc2ba23bf123ae655499a04b645a4
antoniofrancaib <antoniofrancaib@gmail.com>
2025-08-08 11:41:23 -0300

    nw

--- run_pt_swap_flows.sh
+++ run_pt_swap_flows.sh
@@ -34,7 +34,7 @@ mkdir -p logs
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 # --------- User-configurable section ----------
-CONFIG="${CONFIG:-configs/multi_graph_mcmc.yaml}"
+CONFIG="${CONFIG:-configs/AA_simple_mcmc.yaml}"
 TRAIN_MODE="${TRAIN_MODE:-all}"   # all | single
 TEMP_PAIR="${TEMP_PAIR:-0 1}"      # used only if TRAIN_MODE==single
 EPOCHS_OVERRIDE="${EPOCHS:-}"      # optionally override epochs
